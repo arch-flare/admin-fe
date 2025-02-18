3fee88f36525aef5f39b2418067918fd37632943
g-root <brianchegegichuhi@gmail.com>
2025-02-18 03:35:34 +0300

    fix(app): removing logo

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -119,7 +119,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
       >
         {/* <!-- SIDEBAR HEADER --> */}
         <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
-          <Link href="/">
+          {/* <Link href="/">
             <Image
               width={176}
               height={32}
@@ -127,7 +127,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
               alt="Logo"
               priority
             />
-          </Link>
+          </Link> */}
 
           <button
             onClick={() => setSidebarOpen(!sidebarOpen)}
